924f3d9aece036c63a7484b9310b89ee019e019e
Rupaya <mabaig@me.com>
2021-01-07 21:11:24 +0000

    update

---
+++ apollo.sh
@@ -0,0 +1,164 @@
+#! /bin/bash
+
+include () {
+  if [ ! -f mainnet.env ]; then
+    echo "No configuration found, proceeding to setup helper..."
+    cp mainnet.example.env mainnet.env
+    cp example.pwd .pwd
+    echo Welcome to Rupaya Apollo initial configuration helper!
+    read -p "Please enter the name you want to use for your node:"
+    NODENAME=$REPLY
+    sed -i "/NAME/s/=.*/=${NODENAME}/" mainnet.env # Append coinbase
+    read -s -p "Please input a strong password to secure your account:"
+    echo $REPLY > .pwd # Save to .pwd file
+  fi
+  echo "Configuration file found"
+  source mainnet.env
+}
+
+include
+
+initGenesis() {
+  if [ ! -d ${DATADIR}/${NAME}/rupx ]; then
+    echo "No genesis found, creating genesis block..."
+    rupaya --datadir ${DATADIR}/${NAME} init ./config/chain/mainnet/main.json
+    echo
+    echo "${GREEN} Rupaya genesis file initialized ${NC}"
+    echo
+  fi
+}
+
+checkCoinbase() {
+  accounts=$(rupaya --datadir ${DATADIR}/${NAME} account list | awk -F'[{}]' '{print $2}')
+  if [ -z "$accounts" ]
+  then
+    echo
+    echo "No accounts found!"
+    read -p "Would you like to (I)mport an existing account or (C)reate a new one (I/C)? "
+    if [[ $REPLY =~ ^[Ii]$ ]]
+    then
+      import
+    else
+      createNewAccount
+    fi
+  else
+    echo "Account $accounts has been found"
+    accounts=$(rupaya --datadir ${DATADIR}/${NAME} account list | awk -F'[{}]' '{print $2}')
+    get_coinbase=$(echo $accounts | awk '{print $1;}')
+    sed -i "/COINBASE/s/=.*/=${get_coinbase}/" mainnet.env # Append coinbase
+  fi
+}
+
+import() {
+  read -s -p "Enter the private key of the account you want to import:"
+  if [ ${#REPLY} -lt 64 ]
+  then
+    echo "Your private key seems too short. Please start again."
+    exit
+  else
+    echo $REPLY > .tmp
+    rupaya --datadir ${DATADIR}/${NAME} --password .pwd account import .tmp
+    rm .tmp
+    echo
+    echo "Private key successfully imported!"
+    echo
+    accounts=$(rupaya --datadir ${DATADIR}/${NAME} account list | awk -F'[{}]' '{print $2}')
+    get_coinbase=$(echo $accounts | awk '{print $1;}')
+    sed -i "/COINBASE/s/=.*/=${get_coinbase}/" mainnet.env # Append coinbase
+    echo "All accounts found in keystore: "
+    echo
+    echo $accounts
+    echo
+    echo "To remove all excess accounts, please remove them from ${DATADIR}${NAME}/keystore"
+  fi
+}
+
+createNewAccount() {
+  rupaya --datadir ${DATADIR}/${NAME} --password .pwd account new
+  echo
+  get_all_coinbases=$(rupaya --datadir ${DATADIR}/${NAME} account list | awk -F'[{}]' '{print $2}' )
+  get_coinbase=$(echo $get_all_coinbases | awk '{print $1;}')
+  echo
+  echo Address created: $get_coinbase
+  sed -i "/COINBASE/s/=.*/=${get_coinbase}/" mainnet.env # Append coinbase
+}
+
+stop() {
+  echo Stopping rupaya node ${PID}
+  kill -SIGINT ${PID}
+  echo Rupaya node ${PID} stopped.
+}
+
+force () {
+  echo Stopping all rupaya nodes...
+  killall -HUP rupaya
+  echo All rupaya nodes stopped.
+}
+
+run() {
+  # Use this for now.
+  get_all_coinbases=$(rupaya --datadir ${DATADIR}/${NAME} account list | awk -F'[{}]' '{print $2}' )
+  get_coinbase=$(echo $get_all_coinbases | awk '{print $1;}')
+
+  rupaya \
+    --bootnodes "enode://576b502ff97838abf14433608bcdba5b36a708e465bc576e6c226bfe088d110fd267a08a48965540f46fd0a99dc88a44f3d18ac48e94a08e4fcac3a2378d799f@62.171.162.181:10707,enode://6be3ebc8e1305c60c885ac35813179e58500ea6e2b6b2319c631cfde2c9fb7511c11ce65d5b73a7113ad39c12035082c8c42ed5d33fb584a4db2bed35ac6ae23@62.171.167.83:10707,enode://6b06a7b114e4bc3cf76e72d8ad7ad2b241a1afacd653e6a5080e128e75ffc821f6fcf90a0b5956f76c9435d1ed3ac2da7efc88e7c8dd053663a1d2cb991e5741@161.97.93.239:10707" --syncmode "full" --gcmode "archive" \
+    --datadir ${DATADIR}/${NAME} --networkid 499 --port $PORT \
+    --announce-txs \
+    --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 7050 --rpcvhosts "*" \
+    --ws --wsaddr 0.0.0.0 --wsport 8050 --wsorigins "*" \
+    --unlock "$get_coinbase" --password ./.pwd \
+    --ethstats "$NAME:imperial-snowmen-applauded@stats.rupx.io" \
+    --mine --store-reward --verbosity 4 >${DATADIR}/${NAME}/log.txt 2>&1 &
+  process_id=$!
+
+  sed -i "/PID/s/=.*/=${process_id}/" mainnet.env # Write process ID to config for logs
+  echo Rupaya started with process id $process_id
+}
+
+update() {
+  git pull
+}
+
+log() {
+  echo Showing log file. Ctrl+C to exit
+  tail -f -n 2 ${DATADIR}/${NAME}/log.txt
+}
+
+clean() {
+  read -p "This will completely remove any existing data and accounts! Are you sure? (Y/N) "
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    force
+    mkdir -p ./Archive
+    find ./networks/ -name "UTC*" -exec cp {} "./Archive/" \;
+    rm -rf ${DATADIR}
+  else
+    echo "canceled by user."
+    exit
+  fi
+}
+
+start() {
+  sed -i "/COINBASE/s/=.*/=/" mainnet.env
+  initGenesis
+  checkCoinbase
+  wait
+  run
+}
+
+
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied"
+fi
+
+
+case "$1" in
+  start ) start ;;
+  import ) import ;;
+  stop ) stop ;;
+  force-close ) force ;;
+  update ) update ;;
+  clean ) clean ;;
+  log ) log ;;
+esac
